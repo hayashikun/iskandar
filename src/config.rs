e1b87fd52013b9dded5523059d48a87de99c82b0
Ryosuke Hayashi <hayashi@hayashikun.com>
2020-06-10 21:48:34 +0900

    Refactor and add/remove some fields

--- src/config.rs
+++ src/config.rs
@@ -3,25 +3,29 @@ use toml;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Config {
+    pub dry_run: bool,
     pub project_root: String,
-    pub branch: String,
     pub nginx_conf_file: String,
     pub mysql_conf_file: String,
     pub nginx_conf_dir: String,
     pub mysql_conf_dir: String,
     pub deploy_commands: Vec<String>,
+    pub nginx_restart_command: String,
+    pub mysql_restart_command: String,
 }
 
 impl Config {
     pub fn template() -> Config {
         return Config {
-            project_root: String::from(""),
-            branch: String::from("master"),
-            nginx_conf_file: String::from("nginx.conf"),
-            mysql_conf_file: String::from("mysql.cnf"),
-            nginx_conf_dir: String::from("/etc/nginx/conf.d/"),
-            mysql_conf_dir: String::from("/etc/mysql/conf.d/"),
-            deploy_commands: Vec::new(),
+            dry_run: false,
+            project_root: "".to_string(),
+            nginx_conf_file: "nginx.conf".to_string(),
+            mysql_conf_file: "mysql.cnf".to_string(),
+            nginx_conf_dir: "/etc/nginx/conf.d/".to_string(),
+            mysql_conf_dir: "/etc/mysql/conf.d/".to_string(),
+            deploy_commands: vec!["echo Build app".to_string()],
+            nginx_restart_command: "systemctl restart nginx".to_string(),
+            mysql_restart_command: "systemctl restart mysql".to_string(),
         };
     }
 
